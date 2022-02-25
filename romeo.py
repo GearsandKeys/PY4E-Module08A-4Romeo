3ddd6d16218a96397595df3f03dcfca58a4b6922
Nathan Gearke <Nathan.Gearke@veteransunited.com>
2022-02-25 17:42:04 -0600

    Clean up

--- romeo.py
+++ romeo.py
@@ -1,7 +1,5 @@
-def original_word_counter() -> None:    
+def original_word_printer() -> None:    
     file_name = input("Enter file: ")
-
-    # Make sure it prints the list in alphabetical order
 
 
 original_word_counter()
