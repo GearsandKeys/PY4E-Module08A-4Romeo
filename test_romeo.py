0ddb32d590dba283971f9e5aab69bd17495805ed
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2022-12-21 11:50:33 -0600

    Update test_romeo.py

--- test_romeo.py
+++ test_romeo.py
@@ -6,7 +6,7 @@ def test_output(capfd, monkeypatch):
     expected_output = "['Arise', 'But', 'It', 'Juliet', 'Who', 'already', 'and', 'breaks', 'east', 'envious', 'fair', 'grief', 'is', 'kill', 'light', 'moon', 'pale', 'sick', 'soft', 'sun', 'the', 'through', 'what', 'window', 'with', 'yonder']"
     
     monkeypatch.setattr('builtins.input', lambda _:input.pop())
-    romeo.original_word_printer()
+    romeo.original_sorted_word_printer()
     
     out, err = capfd.readouterr()
 
