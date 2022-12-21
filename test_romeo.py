1ecec4fcfd350c5ee837253518bed6e08766b516
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2022-12-21 11:30:37 -0600

    Create test_romeo.py
    Added test file

---
+++ test_romeo.py
@@ -0,0 +1,13 @@
+import romeo
+
+def test_output(capfd, monkeypatch):
+    input = ['romeo.txt']
+
+    expected_output = "['Arise', 'But', 'It', 'Juliet', 'Who', 'already', 'and', 'breaks', 'east', 'envious', 'fair', 'grief', 'is', 'kill', 'light', 'moon', 'pale', 'sick', 'soft', 'sun', 'the', 'through', 'what', 'window', 'with', 'yonder']"
+    
+    monkeypatch.setattr('builtins.input', lambda _:input.pop())
+    romeo.original_word_printer()
+    
+    out, err = capfd.readouterr()
+
+    assert expected_output in out
