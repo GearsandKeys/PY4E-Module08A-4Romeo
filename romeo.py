8481d7bb73083413922b47ce2525396c0a8d94e5
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2022-12-21 11:30:06 -0600

    Update romeo.py

--- romeo.py
+++ romeo.py
@@ -1,5 +1,7 @@
-def original_word_printer() -> None:    
+def original_sorted_word_printer() -> None:    
     file_name = input("Enter file: ")
 
 
-original_word_printer()
+
+if __name__ == "__main__":
+    original_sorted_word_printer()
